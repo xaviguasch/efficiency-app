768281916b0ecfbeec3eda2324a9d896a9724c06
xaviguasch <xaviguasch@gmail.com>
2019-03-20 12:47:39 +0100

    added a new task to the database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,14 +14,34 @@ const User = mongoose.model('User', {
     }
 })
 
-const me = new User({
-    name: 'Martha',
-    age: 25
+// const me = new User({
+//     name: 'Martha',
+//     age: 25
+// })
+
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error!', error);
+
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 })
 
-me.save().then(() => {
-    console.log(me);
+const dishesTask = new Task({
+    description: 'Do the dishes',
+    completed: false
+})
+
+dishesTask.save().then(() => {
+    console.log(dishesTask);
 }).catch((error) => {
     console.log('Error!', error);
-
 })
