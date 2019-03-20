e75518ba5e5d315bcb531cf5e8c312079c97a3a4
xaviguasch <xaviguasch@gmail.com>
2019-03-20 13:16:10 +0100

    validated new users

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,41 +7,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('age must be a positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Martha',
-//     age: 25
-// })
-
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('Error!', error);
-
-// })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
+const you = new User({
+    name: 'Sara',
+    age: 39
 })
 
-const dishesTask = new Task({
-    description: 'Do the dishes',
-    completed: false
-})
-
-dishesTask.save().then(() => {
-    console.log(dishesTask);
+you.save().then(() => {
+    console.log(you);
 }).catch((error) => {
     console.log('Error!', error);
+
 })
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const dishesTask = new Task({
+//     description: 'Do the dishes',
+//     completed: false
+// })
+
+// dishesTask.save().then(() => {
+//     console.log(dishesTask);
+// }).catch((error) => {
+//     console.log('Error!', error);
+// })
