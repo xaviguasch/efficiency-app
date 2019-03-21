33ce78b1c047af3e895f5cf8db1fc2fe5ae47afe
xaviguasch <xaviguasch@gmail.com>
2019-03-21 16:51:10 +0100

    changed the success status for the success to 201

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
     })
@@ -23,7 +23,7 @@ app.post('/taks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
     })
