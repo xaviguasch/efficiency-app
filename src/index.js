8e334e213489d2197f2dc2ca76730d264a68aac8
xaviguasch <xaviguasch@gmail.com>
2019-03-25 15:47:05 +0100

    set up DELETE /tasks/:id route to delete a particular task by ID

--- src/index.js
+++ src/index.js
@@ -79,7 +79,6 @@ app.patch('/users/:id', async (req, res) => {
 
 app.delete('/users/:id', async (req, res) => {
 
-
     try {
         const user = await User.findByIdAndDelete(req.params.id)
 
@@ -89,14 +88,9 @@ app.delete('/users/:id', async (req, res) => {
 
         res.send(user)
 
-
     } catch (e) {
         res.status(500).send()
-
     }
-
-
-
 })
 
 
@@ -172,6 +166,30 @@ app.patch('/tasks/:id', async (req, res) => {
     } catch {
         res.status(500).send()
     }
+})
+
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    } catch (e) {
+        res.status(500).send()
+
+    }
+
+
+
+
+
 })
 
 
