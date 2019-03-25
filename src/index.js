9cd43f5b8a748ece74ce0e4178c527d24daff714
xaviguasch <xaviguasch@gmail.com>
2019-03-25 15:20:31 +0100

    set up PATCH /tasks/:id route to modify a particular task by ID, added validation too

--- src/index.js
+++ src/index.js
@@ -122,6 +122,38 @@ app.get('/tasks/:id', async (req, res) => {
 })
 
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: 'Invalid updates'
+        })
+    }
+
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    } catch {
+        res.status(500).send()
+    }
+
+})
+
+
 
 
 
