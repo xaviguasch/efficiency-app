af5f1ffc24b14fe792a3c1a6af4fc1324eb195e9
xaviguasch <xaviguasch@gmail.com>
2019-03-25 13:57:50 +0100

    added validation, checking so you cannot update a propierty that is not there

--- src/index.js
+++ src/index.js
@@ -47,7 +47,17 @@ app.get('/users/:id', async (req, res) => {
 
 })
 
-app.patch('/users/:id', async (res, res) => {
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: 'Invalid updates'
+        })
+    }
 
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, {
