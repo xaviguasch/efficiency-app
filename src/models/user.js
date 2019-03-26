40e2eb1ca52fb01d92fdef9e668575c88ea9607b
xaviguasch <xaviguasch@gmail.com>
2019-03-26 10:09:31 +0100

    added middleware, right before saving the new user in the database

--- src/models/user.js
+++ src/models/user.js
@@ -40,6 +40,16 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.pre('save', async function (next) {
+    const user = this
+
+
+    console.log('just before saving');
+
+
+    next()
+})
+
 const User = mongoose.model('User', userSchema)
 
 
