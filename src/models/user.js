e9b072eca4053a5326fdb0b2199fbf0f7047f624
xaviguasch <xaviguasch@gmail.com>
2019-03-26 09:42:46 +0100

    added schema to User

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -39,6 +39,8 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+const User = mongoose.model('User', userSchema)
 
 
 module.exports = User
