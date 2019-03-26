f790afc8d0cfe38be0a1651817c7d7e9ee1fe429
xaviguasch <xaviguasch@gmail.com>
2019-03-26 10:34:55 +0100

    saved the hashed password of every new user instead of plain text

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -43,8 +44,11 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     const user = this
 
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
 
-    console.log('just before saving');
+
 
 
     next()
