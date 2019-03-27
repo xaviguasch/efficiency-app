9747d1785d6d7564bba5eb83e6271001b0257356
xaviguasch <xaviguasch@gmail.com>
2019-03-27 13:21:39 +0100

    implemented log-out-all logic, it wipes out all the auth tokens of a particular user

--- src/routers/user.js
+++ src/routers/user.js
@@ -58,6 +58,24 @@ router.post('/users/logout', auth, async (req, res) => {
 
 
 
+// logout all sessions
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+
+    } catch (e) {
+        res.status(500).send()
+    }
+
+
+
+})
+
+
+
 
 
 router.get('/users/me', auth, async (req, res) => {
