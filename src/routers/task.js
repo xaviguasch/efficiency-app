d525ee870f4ea3729501ff70a059f65a9fbc81ab
xaviguasch <xaviguasch@gmail.com>
2019-03-27 19:00:29 +0100

    refactored POST /tasks adding auth & associating task to user id

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -7,8 +8,12 @@ const router = new express.Router()
 
 
 
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
 
     try {
         const savedTask = await task.save()
