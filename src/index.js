91b18ee60a2c27506c12dbde05e2f8c34df41f16
xaviguasch <xaviguasch@gmail.com>
2019-03-27 16:51:36 +0100

    refactored DELETE route for users

--- src/index.js
+++ src/index.js
@@ -32,28 +32,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
-
-
-
-///////////
-
-const jwt = require('jsonwebtoken')
-
-
-
-// const myFunction = async () => {
-//     const token = jwt.sign({
-//         _id: 'abc123'
-//     }, 'thisismynewsecret', {
-//         expiresIn: '7 days'
-//     })
-
-//     console.log(token);
-
-//     const data = jwt.verify(token, 'thisismynewsecret')
-
-//     console.log(data);
-
-// }
-
-// myFunction()
