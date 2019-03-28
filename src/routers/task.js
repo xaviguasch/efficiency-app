c94a8f0d57a1b032800c221bd1506919568bccf0
xaviguasch <xaviguasch@gmail.com>
2019-03-28 14:07:05 +0100

    filtering data in GET /tasks with the completed parameter

--- src/routers/task.js
+++ src/routers/task.js
@@ -25,21 +25,27 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+
+// GET  /tasks?completed=false
 router.get('/tasks', auth, async (req, res) => {
+
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
 
     try {
 
-        // await req.user.populate('tasks').execPopulate()
-        // res.send(req.user.tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
+        res.send(req.user.tasks)
 
-        const tasks = await Task.find({
-            owner: req.user._id
-        })
-        res.send(tasks)
     } catch (e) {
         res.status(500).send()
     }
-
 })
 
 router.get('/tasks/:id', auth, async (req, res) => {
