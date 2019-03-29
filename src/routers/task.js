e4f2392ce55495846581e77194c34ae6605ecd3c
xaviguasch <xaviguasch@gmail.com>
2019-03-29 10:56:16 +0100

    added sorted option to sort by createdAt date

--- src/routers/task.js
+++ src/routers/task.js
@@ -28,13 +28,25 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET  /tasks?completed=false
 // GET /tasks?limit=10&skip=0
+
+// GET /tasks?sortBy=createdAt_asc
 router.get('/tasks', auth, async (req, res) => {
 
     const match = {}
 
+    const sort = {}
+
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+
+    }
+
+
 
     try {
 
@@ -43,7 +55,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
