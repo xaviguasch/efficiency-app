e0181937428b0d7c381908d67e80ca72daa48465
xaviguasch <xaviguasch@gmail.com>
2019-03-29 15:12:47 +0100

    handled errors when uploading files

--- src/routers/user.js
+++ src/routers/user.js
@@ -128,12 +128,26 @@ router.delete('/users/me', auth, async (req, res) => {
 
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an imagine less than 1MG big'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({
+        error: error.message
+    })
 })
 
 
