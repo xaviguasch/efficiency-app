1f327752e170e7f78ac8c8796d6a793fb86daa10
xaviguasch <xaviguasch@gmail.com>
2019-04-01 13:27:19 +0200

    solving bug part 12

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,10 +5,13 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'xaviguasch@gmail.com',
-        subject: 'Thanks for joining in!',
+        from: 'xaviguaschmartorell@gmail.com',
+        subject: 'Thanks for joining in!!!!!',
         text: `Welcome to the app ${name}. Let me know how you get along with the app.`
     })
+
+    console.log('test email');
+
 
 
 }
@@ -16,8 +19,8 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'xaviguasch@gmail.com',
-        subject: 'Sorry to see you go!',
+        from: 'xaviguaschmartorell@gmail.com',
+        subject: 'Sorry to see you go!!!!!',
         text: `Goodbye, ${name}. I hope to see you back sometime soon!`
     })
 }
