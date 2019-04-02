23a499ff409182e6f84f990e3c7b093b5b6cd407
xaviguasch <xaviguasch@gmail.com>
2019-04-02 18:37:36 +0200

    saving a user for test purposes

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,8 +3,16 @@ const app = require('../src/app')
 
 const User = require('../src/models/user')
 
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
 beforeEach(async () => {
     await User.deleteMany()
+
+    await new User(userOne).save()
 })
 
 test('Should signup a new user', async () => {
