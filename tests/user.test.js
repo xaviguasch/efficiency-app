ab73bc9c548bff1a7bd37df0b2abcdf2815db436
xaviguasch <xaviguasch@gmail.com>
2019-04-02 18:28:57 +0200

    added beforeEach function to wipe out the test database before every test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,15 @@
 const request = require('supertest')
 const app = require('../src/app')
 
+const User = require('../src/models/user')
+
+beforeEach(async () => {
+    await User.deleteMany()
+})
+
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
-        name: 'the last fake test',
+        name: 'the last last fake test',
         email: 'fakelast@example.com',
         password: 'MyPass777!'
     }).expect(201)
