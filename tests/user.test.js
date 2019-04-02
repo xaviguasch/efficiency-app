26b665047b057c3dacdda8e6be07a873b3e1adfc
xaviguasch <xaviguasch@gmail.com>
2019-04-02 18:54:59 +0200

    added 'login non-existent user' test

--- tests/user.test.js
+++ tests/user.test.js
@@ -22,3 +22,18 @@ test('Should signup a new user', async () => {
         password: 'MyPass777!'
     }).expect(201)
 })
+
+test('Should log in existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+
+test('Should not login non-existent user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'random string'
+    }).expect(400)
+})
