73156e49a4ae59d38416bd705c8d3424097daf45
xaviguasch <xaviguasch@gmail.com>
2019-04-02 10:00:42 +0200

    added more tests with Jest

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,16 +1,27 @@
 const {
-    calculateTip
+    calculateTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit
 } = require('../src/math')
 
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
-
     expect(total).toBe(13)
+})
+
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
 
 
+test('Should convert 32 F to 0 C', () => {
+    const celcius = fahrenheitToCelsius(32)
+    expect(celcius).toBe(0)
+})
 
-    // if (total !== 13) {
-    //     throw new Error(`total tip should be 13. Got ${total}`)
-    // }
+test('Should convert 0 C to 32 F', () => {
+    const celcius = celsiusToFahrenheit(0)
+    expect(celcius).toBe(32)
 })
