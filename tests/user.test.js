ef0c8d5787fa6ecb5e02723e0aedda357e8927f6
xaviguasch <xaviguasch@gmail.com>
2019-04-03 11:14:04 +0200

    added user testing with auth

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,24 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+
 const app = require('../src/app')
 
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
+
+
 const userOne = {
+    _id: userOneId,
     name: 'Mike',
     email: 'mike@example.com',
-    password: '56what!!'
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({
+            _id: userOneId
+        }, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -36,4 +48,13 @@ test('Should not login non-existent user', async () => {
         email: userOne.email,
         password: 'random string'
     }).expect(400)
+})
+
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
 })
