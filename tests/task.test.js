9893aec8a08e11254a020a9225eede39f7ec503f
xaviguasch <xaviguasch@gmail.com>
2019-04-03 21:15:54 +0200

    set up task test suite

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,6 +1,25 @@
 const request = require('supertest')
+const app = require('../src/app')
 const Task = require('../src/models/task')
+const {
+    userOne,
+    userOneId,
+    setupDatabase
+} = require('./fixtures/db')
 
-test('Should create task for user', () => {
 
+beforeEach(setupDatabase)
+
+test('Should create task for user', async () => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'From my test'
+        })
+        .expect(201)
+
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull()
+    expect(task.completed).toEqual(false)
 })
